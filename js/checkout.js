0da0049e31b1e782091c4d6b9d108c506667e9f9
Thomas Persbråten <83210455+Thomaspersbraaten@users.noreply.github.com>
2021-12-17 15:11:36 +0100

    friday1

--- js/checkout.js
+++ js/checkout.js
@@ -1,9 +1,9 @@
 const continueButton = document.querySelector(".first-continue");
 const shippingInformation = document.querySelector(".shipping-information");
-const secondContinueButton = document.querySelector(".second-continue");
+const shipmentButton = document.querySelector(".shipment-button");
 const paymentInformation = document.querySelector(".payment-information");
 const firstHeader = document.querySelector(".first-header");
-const thirdContinueButton = document.querySelector(".third-continue");
+const paymentButton = document.querySelector(".payment-button");
 const secondHeader = document.querySelector(".second-header");
 const removeItem = document.querySelector(".remove");
 
@@ -15,9 +15,9 @@ const addressField = document.querySelector("#address");
 const addressValidationField = document.querySelector(".address-validation");
 const addressInfo = document.querySelector(".address-info");
 
-const countryField = document.querySelector("#country");
-const countryValidationField = document.querySelector(".country-validation");
-const countryInfo = document.querySelector(".country-info");
+const cityField = document.querySelector("#city");
+const cityValidationField = document.querySelector(".city-validation");
+const cityInfo = document.querySelector(".city-info");
 
 const zipField = document.querySelector("#zipcode");
 const zipValidationField = document.querySelector(".zipcode-validation");
@@ -25,20 +25,55 @@ const zipInfo = document.querySelector(".zipcode-info");
 
 const shippingForm = document.querySelector(".shipping-form");
 
+// payment consts
+
+const cardNumberField = document.querySelector("#card-nr");
+const cardNumberValidationField = document.querySelector(".card-nr-validation");
+const cardNumberInfo = document.querySelector(".card-nr-info");
+
+const cardNameField = document.querySelector("#card-name");
+const cardNameValidationField = document.querySelector(".card-name-validation");
+const cardNameInfo = document.querySelector(".card-name-info");
+
+const expireField = document.querySelector("#expire");
+const expireValidationField = document.querySelector(".expire-validation");
+const expireInfo = document.querySelector(".expire-info");
+
+const cvcField = document.querySelector("#cvc");
+const cvcValidationField = document.querySelector(".cvc-validation");
+const cvcInfo = document.querySelector(".cvc-info");
+
+const paymentForm = document.querySelector(".payment-form");
+
+// review ordre consts
+
+const reviewOrderContainer = document.querySelector(".review-order");
+const reviewDiv = document.querySelector(".review-div");
+
+//
+
 var phoneValid = false;
 var addressValid = false;
-var countryValid = false;
+var cityValid = false;
 var zipValid = false;
 var shippingArray = [];
 
 //
 
-function formValidation(event) {
+var cardNumberValid = false;
+var cardNameValid = false;
+var expireValid = false;
+var cvcValid = false;
+var paymentArray = [];
+
+// shipping form
+
+function shipmentFormValidation(event) {
   event.preventDefault();
-  if (phoneValid && addressValid && countryValid && zipValid) {
+  if (phoneValid && addressValid && cityValid && zipValid) {
     shippingArray.push(phoneField.value);
     shippingArray.push(addressField.value);
-    shippingArray.push(countryField.value);
+    shippingArray.push(cityField.value);
     shippingArray.push(zipField.value);
     console.log(shippingArray);
     shippingForm.reset();
@@ -55,32 +90,74 @@ function formValidation(event) {
   } else {
     phonevalidation();
     addressValidation();
-    countryValidation();
+    cityValidation();
     zipValidation();
   }
 }
-shippingForm.addEventListener("submit", formValidation);
+shippingForm.addEventListener("submit", shipmentFormValidation);
 
-//
+// payment form
+
+function paymentFormValidation(event) {
+  event.preventDefault();
+  if (cardNumberValid && cardNameValid && expireValid && cvcValid) {
+    paymentArray.push(cardNameField.value);
+    paymentArray.push(cardNumberField.value);
+
+    console.log(paymentArray);
+    paymentForm.reset();
+    paymentInformation.style.display = "none";
+    // reviewOrder.style.display = "none";
+    secondHeader.innerHTML = `2. Payment Information ✔️`;
+    createReviewOrder();
+
+    return paymentArray;
+  } else {
+    cardNumberValidation();
+    cardNameValidation();
+    expireValidation();
+    cvcValidation();
+  }
+}
+paymentForm.addEventListener("submit", paymentFormValidation);
+const reviewInfo = document.querySelector(".review-info");
+
+// review order HTML
+
+function createReviewOrder() {
+  reviewOrderContainer.style.display = "flex";
+  reviewInfo.innerHTML = `
+  <h2> Your shipment will be delivered to your address</h2> 
+  <h3> ${shippingArray[1]}</h3>
+  <h3> ${shippingArray[2]} ${shippingArray[3]}</h3>
+  <h3> Phone: ${shippingArray[0]}</h3>
+  <h2> Payed with credit card: </h2>
+    <h3> Card Owner: ${paymentArray[0]}</h3>
+    <h3> Card Number: ${paymentArray[1]}</h3>
+
+    `;
+}
+
+// step by step confirmations
 
 function showShipmentInformation() {
   shippingInformation.style.display = "flex";
 }
 
-function showPaymentInformation() {
-  paymentInformation.style.display = "flex";
-  shippingInformation.style.display = "none";
-  firstHeader.innerHTML = `1. Shipping Information ✔️`;
-}
+// function showPaymentInformation() {
+//   paymentInformation.style.display = "flex";
+//   shippingInformation.style.display = "none";
+//   firstHeader.innerHTML = `1. Shipping Information ✔️`;
+// }
 
-function reviewOrder() {
-  paymentInformation.style.display = "none";
-  secondHeader.innerHTML = "2. Payment Information ✔️";
-}
+// function reviewOrder() {
+//   paymentInformation.style.display = "none";
+//   secondHeader.innerHTML = "2. Payment Information ✔️";
+// }
 
 continueButton.addEventListener("click", showShipmentInformation);
-// secondContinueButton.addEventListener("click", showPaymentInformation);
-thirdContinueButton.addEventListener("click", reviewOrder);
+
+// shipment functions
 
 function phonevalidation() {
   phoneValidationField.innerHTML = "";
@@ -93,9 +170,10 @@ function phonevalidation() {
     addInputInformation(phoneInfo);
   }
 }
+
 function addressValidation() {
   addressValidationField.innerHTML = "";
-  if (addressField.textLength >= 1) {
+  if (checkLength(addressField.value, 1)) {
     greenValidationStatus(addressValidationField, addressField);
     removeInputInformation(addressInfo);
     addressValid = true;
@@ -104,15 +182,16 @@ function addressValidation() {
     addInputInformation(addressInfo);
   }
 }
-function countryValidation() {
-  countryValidationField.innerHTML = "";
-  if (countryField.textLength >= 1) {
-    greenValidationStatus(countryValidationField, countryField);
-    removeInputInformation(countryInfo);
-    countryValid = true;
+
+function cityValidation() {
+  cityValidationField.innerHTML = "";
+  if (checkLength(cityField.value, 1)) {
+    greenValidationStatus(cityValidationField, cityField);
+    removeInputInformation(cityInfo);
+    cityValid = true;
   } else {
-    redValidationStatus(countryValidationField, countryField);
-    addInputInformation(countryInfo);
+    redValidationStatus(cityValidationField, cityField);
+    addInputInformation(cityInfo);
   }
 }
 
@@ -140,6 +219,81 @@ function checkZip(zip) {
   return zipMAtch;
 }
 
+function checkLength(value, len) {
+  if (value.trim().length >= len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// payment functions
+
+function cardNumberValidation() {
+  cardNumberValidationField.innerHTML = "";
+  if (checkCardNumber(cardNumberField.value)) {
+    greenValidationStatus(cardNumberValidationField, cardNumberField);
+    removeInputInformation(cardNumberInfo);
+    cardNumberValid = true;
+  } else {
+    redValidationStatus(cardNumberValidationField, cardNumberField);
+    addInputInformation(cardNumberInfo);
+  }
+}
+
+function cardNameValidation() {
+  cardNameValidationField.innerHTML = "";
+  if (checkLength(cardNameField.value, 1)) {
+    greenValidationStatus(cardNameValidationField, cardNameField);
+    removeInputInformation(cardNameInfo);
+    cardNameValid = true;
+  } else {
+    redValidationStatus(cardNameValidationField, cardNameField);
+    addInputInformation(cardNameInfo);
+  }
+}
+
+function expireValidation() {
+  expireValidationField.innerHTML = "";
+  if (checkExpire(expireField.value)) {
+    greenValidationStatus(expireValidationField, expireField);
+    removeInputInformation(expireInfo);
+    expireValid = true;
+  } else {
+    redValidationStatus(expireValidationField, expireField);
+    addInputInformation(expireInfo);
+  }
+}
+
+function cvcValidation() {
+  cvcValidationField.innerHTML = "";
+  if (checkcvc(cvcField.value)) {
+    greenValidationStatus(cvcValidationField, cvcField);
+    removeInputInformation(cvcInfo);
+    cvcValid = true;
+  } else {
+    redValidationStatus(cvcValidationField, cvcField);
+    addInputInformation(cvcInfo);
+  }
+}
+
+function checkExpire(expire) {
+  const regex = /\b\d{4}\b/;
+  const cardMatch = regex.test(expire);
+  return cardMatch;
+}
+
+function checkcvc(cvc) {
+  const regex = /\b\d{3}\b/;
+  const cvcMatch = regex.test(cvc);
+  return cvcMatch;
+}
+
+function checkCardNumber(cardnumber) {
+  const regex = /\b\d{16}\b/;
+  const cardMatch = regex.test(cardnumber);
+  return cardMatch;
+}
 // loader and green checkmark/red cross validation + border
 
 function greenValidationStatus(container, input) {
@@ -174,8 +328,40 @@ function removeInputInformation(container) {
   }, 400);
 }
 
-// events
+//  shipment events
 phoneField.addEventListener("blur", phonevalidation);
 addressField.addEventListener("blur", addressValidation);
-countryField.addEventListener("blur", countryValidation);
+cityField.addEventListener("blur", cityValidation);
 zipField.addEventListener("blur", zipValidation);
+
+// payment events
+
+cardNumberField.addEventListener("blur", cardNumberValidation);
+cardNameField.addEventListener("blur", cardNameValidation);
+expireField.addEventListener("blur", expireValidation);
+cvcField.addEventListener("blur", cvcValidation);
+
+// confirm
+
+const confirmButton = document.querySelector(".confirm-button");
+const checkoutContainer = document.querySelector(".checkout");
+const basketContainer = document.querySelector(".basket_contents");
+
+function createConfirmationInfo() {
+  checkoutContainer.style.display = "none";
+  reviewDiv.style.display = "none";
+  basketContainer.innerHTML = `
+  <h1>Order received!</h1>
+  <div class="order__info">
+    <p>Your order has been received.</p>
+    <p>Order nr: 1894</p>
+    <p>You will receive an email confirmation shortly.</p>
+  </div>
+  <div class="go__home">
+    <h2>Click here to go back to the home page</h2>
+    <a href="loggedin.html" class="cta home_button">Home</a>
+  </div>
+  `;
+}
+
+confirmButton.addEventListener("click", createConfirmationInfo);
